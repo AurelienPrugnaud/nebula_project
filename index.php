cf8b3a711f0cdc5d7ec5491a23073405caaa8200
AurelienPrugnaud <aurelienpru@gmail.com>
2022-01-17 12:55:58 +0100

    Forth commit: start coding

--- index.php
+++ index.php
@@ -5,12 +5,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nebula</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <header>
-
+    <header class="d-flex justify-content-between align-items-center p-3">
+        <div class="ms-2 d-flex">
+            <div class="me-5">
+                <a href="">
+                    <img class="logo" src="assets/img/logo/logo_nebula_no_name_50px.png" alt="logo nebula">
+                    <img class="logo" src="assets/img/logo/logo_nebula_name_100px.png" alt="logo nebula">
+                </a>
+            </div>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Chercher" aria-label="Search">
+                <button class="btn btn-outline-primary" type="submit">Chercher</button>
+            </form>
+        </div>
+        <div class="me-2">
+            <span>
+                <a href="">Boutique</a> 
+            </span>
+            <span>
+                <a href="">Compte</a>
+            </span>
+            <span>
+                <a href="">Connexion</a>
+            </span>
+        </div>
     </header>
-    <main>
+    <main class="container">
         <p>
             blabla
         </p>
@@ -19,4 +43,5 @@
 
     </footer>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </html>
