8e817773bc50823511678a8e624f01efe0027729
AurelienPrugnaud <aurelienpru@gmail.com>
2022-01-17 17:00:30 +0100

    fifth commit

--- index.php
+++ index.php
@@ -9,11 +9,13 @@
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <header class="d-flex justify-content-between align-items-center p-3">
+
+    <! -- //////////////////////////////HEADER START//////////////////////////////////////// -->
+    <header class="sticky-top d-flex justify-content-between align-items-center p-3">
         <div class="ms-2 d-flex">
-            <div class="me-5">
+            <div>
                 <a href="">
-                    <img class="logo" src="assets/img/logo/logo_nebula_no_name_50px.png" alt="logo nebula">
+                    <img class="logo" src="assets/img/logo/logo_nebula_no_name_55px.png" alt="logo nebula">
                     <img class="logo" src="assets/img/logo/logo_nebula_name_100px.png" alt="logo nebula">
                 </a>
             </div>
@@ -23,25 +25,129 @@
             </form>
         </div>
         <div class="me-2">
-            <span>
-                <a href="">Boutique</a> 
-            </span>
-            <span>
-                <a href="">Compte</a>
-            </span>
-            <span>
-                <a href="">Connexion</a>
-            </span>
+                <a class="menu" href="">Boutique</a> 
+                <a class="menu" href="">Compte</a>
+                <a class="menu" href="">Connexion</a>
         </div>
     </header>
-    <main class="container">
-        <p>
-            blabla
-        </p>
-    </main>
-    <footer>
+    <! -- //////////////////////////////HEADER END//////////////////////////////////////// -->
 
+    <! -- //////////////////////////////MAIN START//////////////////////////////////////// -->
+    <main class="container">
+
+        <! -- //////////////////////////////CAROUSEL START//////////////////////////////////////// -->
+        <div class="section">
+            <img class="carousel" src="assets/img/games/god_of_war.jpg" alt="god of war">
+        </div>
+        <! -- //////////////////////////////CAROUSEL END//////////////////////////////////////// -->
+
+        <! -- //////////////////////////////DISCOVERIES START//////////////////////////////////////// -->
+        <div class="section">
+            <div class="d-flex align-items-center mb-3">
+                <h1 class="t-section me-3 ">Découvertes</h1>
+                <a href="">
+                    <button class="btn btn-outline-primary">Voir plus</button>
+                </a>
+            </div>
+            <div class="d-flex">
+                <div class="border-discoveries-card">
+                    <div class="discoveries-card">
+                        <img class="img-discoveries" src="assets/img/games/final_fantasy_intergrade.jpeg" alt="">
+                        <h2 class="t-discoveries">FINAL FANTASY® VII REMAKE INTERGRADE</h2>
+                        <p class="">Le classique intemporel FINAL FANTASY® VII REMAKE INTERGRADE est de retour avec une aventure, des graphismes et un système de combat inédits.</p>
+                        <p class="">79,99 €</p>
+                    </div>
+                </div>
+                    <div class="border-discoveries-card">
+                        <div class="discoveries-card">
+                        <img class="img-discoveries" src="assets/img/games/Kena_BOS_GameArt.jpg" alt="">
+                        <h2 class="t-discoveries">Kena: Bridge of Spirits</h2>
+                        <p class="">Incarnez Kena, partez à l’aventure et développez une équipe de compagnons-esprits charmants appelés les Rots. </p>
+                        <p class="">39,99 €</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <! -- //////////////////////////////DISCOVERIES END//////////////////////////////////////// -->
+
+        <! -- //////////////////////////////MIDSECTION START//////////////////////////////////////// -->
+        <div class="section d-flex">
+            <div>
+                <div class="d-flex align-items-center mb-3">
+                    <h1 class="t-section me-3 ">Nouvelles sorties</h1>
+                    <a href="">
+                        <button class="btn btn-outline-primary">Voir plus</button>
+                    </a>
+                </div>
+                <div class="d-flex align-items-center">
+                    <img class="img-midsection me-3" src="assets/img/games/god_of_war.jpg" alt="">
+                    <div>
+                        <h2>God of War</h2>
+                        <p>49,99 €</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <! -- //////////////////////////////MIDSECTION END//////////////////////////////////////// -->
+
+        <! -- //////////////////////////////DISCOVERIES START//////////////////////////////////////// -->
+        <div class="section">
+            <div class="d-flex align-items-center mb-3">
+                <h1 class="t-section me-3 ">Découvertes</h1>
+                <a href="">
+                    <button class="btn btn-outline-primary">Voir plus</button>
+                </a>
+            </div>
+            <div class="d-flex">
+                <div class="border-discoveries-card">
+                    <div class="discoveries-card">
+                        <img class="img-discoveries" src="assets/img/games/final_fantasy_intergrade.jpeg" alt="">
+                        <h2 class="t-discoveries">FINAL FANTASY® VII REMAKE INTERGRADE</h2>
+                        <p class="">Le classique intemporel FINAL FANTASY® VII REMAKE INTERGRADE est de retour avec une aventure, des graphismes et un système de combat inédits.</p>
+                        <p class="">79,99 €</p>
+                    </div>
+                </div>
+                    <div class="border-discoveries-card">
+                        <div class="discoveries-card">
+                        <img class="img-discoveries" src="assets/img/games/Kena_BOS_GameArt.jpg" alt="">
+                        <h2 class="t-discoveries">Kena: Bridge of Spirits</h2>
+                        <p class="">Incarnez Kena, partez à l’aventure et développez une équipe de compagnons-esprits charmants appelés les Rots. </p>
+                        <p class="">39,99 €</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <! -- //////////////////////////////DISCOVERIES END//////////////////////////////////////// -->
+
+    </main>
+    <! -- //////////////////////////////MAIN END//////////////////////////////////////// -->
+
+    <! -- //////////////////////////////FOOTER START//////////////////////////////////////// -->
+    <footer class="pt-5 pb-5 ps-4 pe-4">
+        <div class="mb-3">
+            <span class="iconify me-3" data-icon="akar-icons:instagram-fill" style="color: white; font-size: 30px"></span>
+            <span class="iconify" data-icon="akar-icons:twitter-fill" style="color: white; font-size: 30px;"></span>
+        </div>
+        <div>
+            <p>
+            © 2022, Nebula. Tous droits réservés. Nebula et le logo Nebula sont des marques ou des marques déposées de Nebula en France et ailleurs. Les autres marques ou noms de produit appartiennent à leurs propriétaires respectifs. En dehors de la France, les transactions sont gérées par Nebula, S.à r.l.   
+            </p>
+        </div>
+        <div class="d-flex justify-content-between ">
+            <div>
+                <a class="menu me-2" href="">Contact</a>
+                <a class="menu me-2" href="">Conditions d'utilisations</a>
+                <a class="menu me-2" href="">Politique de confidentialité</a>
+            </div>
+            <div class="">
+                <img class="logo" src="assets/img/logo/logo_nebula_no_name_55px.png" alt="logo nebula">
+                <img class="logo" src="assets/img/logo/logo_nebula_name_100px.png" alt="logo nebula">  
+            </div>   
+        </div>
     </footer>
+    <! -- //////////////////////////////FOOTER END//////////////////////////////////////// -->
+
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://code.iconify.design/2/2.1.0/iconify.min.js"></script>
 </html>
